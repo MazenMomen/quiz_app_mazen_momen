061838ad2f2c6359f4a87481a0a3ed558ac74991
Mazen-4 <mazenmoamen02@gmail.com>
2023-08-13 09:19:01 +0300

    Making opening screen responsive & navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app_mazen_momen/screens/login_screen.dart';
+import 'package:quiz_app_mazen_momen/screens/opening_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home: const OpeningScreen(),
     );
   }
 }
