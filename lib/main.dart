e2408b3c884c68f36c71eccf9a34a13bbdfc1b08
Mazen-4 <mazenmoamen02@gmail.com>
2023-08-19 15:02:54 +0300

    converting login screen to a stack to prevent overflow

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app_mazen_momen/screens/result_screen.dart';
+import 'package:quiz_app_mazen_momen/screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
-      home: const ResultScreen(),
+      home: const LoginScreen(),
     );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,177 +8,159 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 61, 212, 184),
-      body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.31,
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              padding: const EdgeInsets.symmetric(),
+              height: MediaQuery.of(context).size.height * 0.4,
               width: MediaQuery.of(context).size.width,
+              color: const Color.fromARGB(255, 61, 212, 184),
+              child: Center(
+                child: Image.asset("assets/ghost.png",
+                    width: MediaQuery.of(context).size.width * 0.35,
+                    height: MediaQuery.of(context).size.height * 0.3),
+              ),
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * 2 / 3,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Container(height: MediaQuery.of(context).size.height * 0.02),
-                  Image.asset("assets/ghost.png",
-                      height: MediaQuery.of(context).size.height * 0.18),
+                  const Spacer(),
+                  const Text(
+                    "Login",
+                    style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold),
+                  ),
+                  const Spacer(),
+                  const TextField(
+                      decoration: InputDecoration(
+                    hintText: "Username",
+                    prefixIcon: Icon(Icons.person),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                  )),
+                  const Spacer(),
+                  const TextField(
+                      decoration: InputDecoration(
+                    hintText: "Password",
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: Icon(Icons.visibility),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                  )),
+                  const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text("New to quiz app? "),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Register",
+                            style: GoogleFonts.roboto(
+                              color: const Color.fromARGB(255, 61, 212, 184),
+                            ),
+                          )),
+                    ],
+                  ),
+                  const Spacer(),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.35,
+                    height: MediaQuery.of(context).size.height * 0.04,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey
+                              .withOpacity(0.4), // Set the shadow color
+                          spreadRadius:
+                              6, // Set the spread radius of the shadow
+                          blurRadius: 6, // Set the blur radius of the shadow
+                          offset: const Offset(
+                              0, 6), // Set the offset of the shadow
+                        ),
+                      ],
+                      borderRadius:
+                          BorderRadius.circular(50), // Set button border radius
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute<void>(
+                            builder: (BuildContext context) =>
+                                const CategoryScreen(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            const Color.fromARGB(255, 61, 212, 184),
+                      ),
+                      child: Text(
+                        "Login",
+                        style: GoogleFonts.roboto(
+                          color: Colors.white,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  Image.asset("assets/fingerprint.png",
+                      height: MediaQuery.of(context).size.height * 0.05),
+                  const Spacer(),
+                  Text(
+                    "Use Touch ID",
+                    style: GoogleFonts.roboto(
+                      color: Colors.black.withOpacity(0.4),
+                    ),
+                  ),
+                  const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Checkbox(
+                            value: true,
+                            onChanged: (value) {},
+                            fillColor: MaterialStateProperty.all(
+                                const Color.fromARGB(255, 61, 212, 184)),
+                          ),
+                          const Text("Remember me"),
+                        ],
+                      ),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Forgot Password?",
+                            style: GoogleFonts.roboto(
+                                color: const Color.fromARGB(255, 61, 212, 184),
+                                fontWeight: FontWeight.bold),
+                          )),
+                    ],
+                  ),
                 ],
               ),
             ),
-            Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.01,
-                      ),
-                      const Text(
-                        "Login",
-                        style: TextStyle(
-                            fontSize: 38, fontWeight: FontWeight.bold),
-                      ),
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.02,
-                      ),
-                      const TextField(
-                          decoration: InputDecoration(
-                        hintText: "Username",
-                        prefixIcon: Icon(Icons.person),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(30)),
-                        ),
-                      )),
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.01,
-                      ),
-                      const TextField(
-                          decoration: InputDecoration(
-                        hintText: "Password",
-                        prefixIcon: Icon(Icons.lock),
-                        suffixIcon: Icon(Icons.visibility),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(30)),
-                        ),
-                      )),
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.02,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          const Text("New to quiz app? "),
-                          TextButton(
-                              onPressed: () {},
-                              child: Text(
-                                "Register",
-                                style: GoogleFonts.roboto(
-                                  color:
-                                      const Color.fromARGB(255, 61, 212, 184),
-                                ),
-                              )),
-                        ],
-                      ),
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.04,
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.35,
-                        height: MediaQuery.of(context).size.height * 0.04,
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey
-                                  .withOpacity(0.4), // Set the shadow color
-                              spreadRadius:
-                                  6, // Set the spread radius of the shadow
-                              blurRadius:
-                                  6, // Set the blur radius of the shadow
-                              offset: const Offset(
-                                  0, 6), // Set the offset of the shadow
-                            ),
-                          ],
-                          borderRadius: BorderRadius.circular(
-                              50), // Set button border radius
-                        ),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute<void>(
-                                builder: (BuildContext context) =>
-                                    const CategoryScreen(),
-                              ),
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                const Color.fromARGB(255, 61, 212, 184),
-                          ),
-                          child: Text(
-                            "Login",
-                            style: GoogleFonts.roboto(
-                              color: Colors.white,
-                              fontSize: 14,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.03,
-                      ),
-                      Image.asset("assets/fingerprint.png",
-                          height: MediaQuery.of(context).size.height * 0.05),
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.01,
-                      ),
-                      Text(
-                        "Use Touch ID",
-                        style: GoogleFonts.roboto(
-                          color: Colors.black.withOpacity(0.4),
-                        ),
-                      ),
-                      const Spacer(),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Checkbox(
-                                value: true,
-                                onChanged: (value) {},
-                                fillColor: MaterialStateProperty.all(
-                                    const Color.fromARGB(255, 61, 212, 184)),
-                              ),
-                              const Text("Remember me"),
-                            ],
-                          ),
-                          TextButton(
-                              onPressed: () {},
-                              child: Text(
-                                "Forgot Password?",
-                                style: GoogleFonts.roboto(
-                                    color:
-                                        const Color.fromARGB(255, 61, 212, 184),
-                                    fontWeight: FontWeight.bold),
-                              )),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
